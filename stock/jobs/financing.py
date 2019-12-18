1008121fcb0b8feffd1b7d3352c08e5080228307
Box <forkome@gmail.com>
2019-12-19 00:50:27 +0800

    fixed

--- stock/jobs/financing.py
+++ stock/jobs/financing.py
@@ -15,13 +15,13 @@ class Financing(object):
         self.open_conn()
 
         self.cur = self.conn.cursor()
-        self.cur.execute("select stock_no,stock_name,round(a.today_borrow_stock/a.today_borrow_money,2)*100 percent from financing a where round(a.today_borrow_stock/a.today_borrow_money,2)*100 >= 80")
+        self.cur.execute("select stock_no,stock_name,round(a.today_borrow_stock/a.today_borrow_money,2)*100 percent,data_date from financing a where round(a.today_borrow_stock/a.today_borrow_money,2)*100 >= 80")
         if self.cur.rowcount > 0:
             rows = self.cur.fetchall()
             token = "zoQSmKALUqpEt9E7Yod14K9MmozBC4dvrW1sRCRUMOU"
             for row in rows:
-                msg = "Stock No :{stock_no}({stock_name}) 目前資券比{percent}超過80%"
-                msg = msg.format(stock_no=row[0], stock_name=row[1], percent=row[2])
+                msg = "Stock No :{data_date} {stock_no}({stock_name}) 目前資券比{percent}超過80%"
+                msg = msg.format(stock_no=row[0], stock_name=row[1], percent=row[2], data_date=row[3])
                 lineNotifyMessage(token, msg)
 
 financing = Financing()
