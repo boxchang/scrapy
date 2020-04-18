bbde1f8c176da0ed1c15cc8cfcd6c9500c012ed1
Box <forkome@gmail.com>
2020-04-18 22:03:43 +0800

    update

--- web104/run.sh
+++ web104/run.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 
-cd /src/scrapy/web104
+#cd /src/scrapy/web104
 
-scrapy crawl web104
+#scrapy crawl web104
 
 cd /src/scrapy/web104/jobs
+
+python getweb104_s.py
+
+python getweb104_w.py
 
 python fetchdata.py
 
