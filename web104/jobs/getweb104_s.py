24f9203f195ee8d959ee67663fe4aa59bb187853
box_chang <forkome@gmail.com>
2020-04-27 16:27:37 +0800

    update

--- web104/jobs/getweb104_s.py
+++ web104/jobs/getweb104_s.py
@@ -117,44 +117,43 @@ while i < len(List):
     # print('正在處理第' + str(i) + '筆，共 ' + str(len(List)) + ' 筆資料')
     content = List[i]
     # 這裡用Try的原因是，有時候爬太快會遭到系統阻擋導致失敗。因此透過這個方式，當我們遇到錯誤時，會重新再爬一次資料！
-    try:
-        #content.attrs['href'].strip('//') => www.104.com.tw/job/5cmwg?jobsource=hotjob_chr
-        #www.104.com.tw/job/ajax/similarJobs/5cmwg
-        batchNo = time.strftime("%Y%m%d%H%M%S", time.localtime())
-        url = 'https://' + content.attrs['href'].strip('//')
-        if url.find("www.104.com.tw") > 0:
-            jobNo = url[url.rfind("/") + 1:url.rfind("?")]
-            ajax_url = "https://www.104.com.tw/job/ajax/content/" + jobNo
-            res = requests.get(ajax_url)
-            job = json.loads(res.text)
-            item = {}
-            item['custName'] = job['data']['header']['custName']
-            item['jobNo'] = jobNo
-            item['jobName'] = job['data']['header']['jobName']
-            item['description'] = job['data']['jobDetail']['jobDescription']
-            item['jobAddrNoDesc'] = job['data']['jobDetail']['addressRegion']
-            item['jobLink'] = url
-            item['addr'] = job['data']['jobDetail']['addressDetail']
-            item['history'] = job['data']['condition']['workExp']
-            item['tool'] = ''
-            item['other'] = job['data']['condition']['other']
-            item['benefit'] = job['data']['welfare']['welfare']
-            item['update_date'] = job['data']['header']['appearDate']
-            item['batchNo'] = batchNo
-            print(item)
 
-            if validate(jobNo,conn):
-                insertTable(item,conn)
+    #content.attrs['href'].strip('//') => www.104.com.tw/job/5cmwg?jobsource=hotjob_chr
+    #www.104.com.tw/job/ajax/similarJobs/5cmwg
+    batchNo = time.strftime("%Y%m%d%H%M%S", time.localtime())
+    url = 'https://' + content.attrs['href'].strip('//')
+    if url.find("www.104.com.tw") > 0:
+        jobNo = url[url.rfind("/") + 1:url.rfind("?")]
+        ajax_url = "https://www.104.com.tw/job/ajax/content/" + jobNo
+        res = requests.get(ajax_url)
+        job = json.loads(res.text)
+        item = {}
+        item['custName'] = job['data']['header']['custName']
+        item['jobNo'] = jobNo
+        item['jobName'] = job['data']['header']['jobName']
+        item['description'] = job['data']['jobDetail']['jobDescription']
+        item['jobAddrNoDesc'] = job['data']['jobDetail']['addressRegion']
+        item['jobLink'] = url
+        item['addr'] = job['data']['jobDetail']['addressDetail']
+        item['history'] = job['data']['condition']['workExp']
+        item['tool'] = ''
+        item['other'] = job['data']['condition']['other']
+        item['benefit'] = job['data']['welfare']['welfare']
+        item['update_date'] = job['data']['header']['appearDate']
+        item['batchNo'] = batchNo
+        print(item)
 
-            i += 1
-            print("Success and Crawl Next 目前正在爬第" + str(i) + "個職缺資訊")
-        else:
-            i += 1
-            continue
+        if validate(jobNo,conn):
+            insertTable(item,conn)
 
-        time.sleep(0.5)  # 執行完休息0.5秒，避免造成對方主機負擔
-    except:
-        print("Fail and Try Again!")
+        i += 1
+        print("Success and Crawl Next 目前正在爬第" + str(i) + "個職缺資訊")
+    else:
+        i += 1
+        continue
+
+    time.sleep(0.5)  # 執行完休息0.5秒，避免造成對方主機負擔
+
 
 
 conn.close()
